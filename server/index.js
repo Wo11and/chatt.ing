49a712ccbb6ab33a37129a26e77c8d53341ad189
Tsvetan <tzpet9@gmail.com>
2023-11-13 20:45:26 +0200

    Intitial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+import "dotenv/config";
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.send({ message: "Hello World!" });
+});
+
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
