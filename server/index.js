f4e59a067ff0c86cc5d3227598560686bbcfa4f9
Tsvetan <tzpet9@gmail.com>
2023-11-18 18:23:12 +0200

    Added socketio library

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
 import express from "express";
 import cors from "cors";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 import "dotenv/config";
 
 const app = express();
+app.use(cors());
+const server = createServer(app);
+const io = new Server(server, {
+	cors: {
+		origin: process.env.CLIENT_ADRESS,
+	},
+});
+
 const port = process.env.SERVER_PORT;
 
 app.use(express.json());
-app.use(cors());
 
 app.get("/", (req, res) => {
 	res.send({ message: "Hello World!" });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
+});
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
 });
