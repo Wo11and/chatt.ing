211ede66c923207c125301ee4821075658a1cf32
kaisiq <dahudohu@gmail.com>
2023-11-20 19:06:32 +0200

    Install socketio

--- server/index.js
+++ server/index.js
@@ -1,17 +1,44 @@
 import express from "express";
 import cors from "cors";
-
+import {Server} from "socket.io";
 import "dotenv/config";
+import { AuthenticationService } from "./services/authenticate.js";
+
 
 const app = express();
 const port = process.env.SERVER_PORT;
+const io = new Server(5000);
+const auth = new AuthenticationService;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-	res.send({ message: "Hello World!" });
+	res.sendFile('/home/kaisiq/Programming/chatt.ing/frontend/index.html');
 });
+
+
+app.post("/login", (req,res,next) => {
+	auth.loginMiddleware(req,res,next);
+});
+
+
+app.post("/register", (req,res,next) => {
+	auth.registerMiddleware(req,res,next)
+});
+
+
+io.on('connection', function(socket){
+	console.log('user connected');
+	socket.on('chat message', function(msg){
+	  io.emit('chat message', msg);
+	});
+	socket.on('disconnect', function(){
+	  console.log('user disconnected');
+	});
+});
+
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
--- server/services/authenticate.js
+++ server/services/authenticate.js
@@ -1,33 +1,139 @@
 import jsonwebtoken from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import Knex from 'knex';
+import dotenv from "dotenv";
 
+ 
+
+dotenv.config();
+
+const config = {
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    ssl: false,
+  },
+};
+const pg = Knex(config);
+  
+
+  
 const webTokenSecret = process.env.WEB_TOKEN_SECRET;
+const bcrypt_saltRounds = 10;
+
+
 
 export class AuthenticationService{
 
-    encryptPassword(){
-        return;
-    }
-    
-    async register(username, password){
-        //TODO: check if username exists
-        encrpytedPassword = encryptPassword(password);
-        //TODO: add user with pass to db
 
-        //id -> db id
-        return generateWebToken(username, id);
+    checkTokenMiddleware = (req, res, next) => {
+        const header = req.headers['authorization'];
+    
+        if(typeof header !== 'undefined') {
+            const bearer = header.split(' ');
+            const token = bearer[1];
+    
+            req.token = token;
+            next();
+        } else {
+            //If header is undefined return Forbidden (403)
+            res.sendStatus(403)
+        }
+    }
+
+    registerMiddleware(req, res, next){
+        const username = req.body.username;
+        const password = req.body.password;
+        try{
+            const token = this.register(username, password);
+            res.send(token);
+            res.end();  
+            next();
+        }
+        catch(err){
+            console.log(err);
+            console.log("cant register");
+            res.writeHead(400);
+        }
+    }
+
+
+    loginMiddleware(req, res, next){
+        const username = req.body.username;
+        const password = req.body.password;
+        try{
+            const token = this.login(username,password);
+            res.send(token);
+        }
+        catch(err){
+            console.log(err);
+            console.log("cant login");
+        }
+    }
+
+
+    async register(username, password){
+        try{
+            //connect to db
+            await pg.raw('SELECT 1');
+            const user = await pg('users').where('username', username).first();
+            if (user !== undefined){
+                // If username exists, return an error or throw an exception
+                throw new Error('Username already exists');
+            }
+            const hashedPassword = await bcrypt.hash(password, bcrypt_saltRounds);
+            // Store hash and username in DB
+            //...
+            const [userId] = await pg('users').insert({
+                username,
+                password: hashedPassword,
+            }, 'username');
+            console.log("Created user: " + userId);
+            const token = jsonwebtoken.sign({username}, webTokenSecret, 
+                { algorithm: 'RS256' },{ expiresIn: "1h" });
+            return token;
+        } catch (error) {
+            // Handle any errors that occur during registration
+            console.error('Error during registration:', error.message);
+            throw error;
+        }
+    
     }
 
     async login(username, password){
-        //TODO: check if username exists
+        try{
+        //TODO: check if username exists -> if not - error
+        const user = await pg('users').where('username', username).first();
+        if (!user) {
+            // If username exists, return an error or throw an exception
+            throw new Error('Username doesnt exist');
+        }
         //if exists check pass if not error
-        //if pass wrong err
+        // hash == db password
+
+        bcrypt.compare(password, user.password, function(err, result) {
+            // result == true/false
+            if(err){
+                console.log(err);
+                throw err;
+            }
+            if(!result){
+                throw new Error("Password not matching");
+            }
+        });
 
         //id -> db id
-        return generateWebToken(username, id);
-    }
+        return generateWebToken(username);
+        }
+        catch(err){
+            console.log("Error during login: ", err);
+            throw err;
+        }
 
-    async generateWebToken(username, id){
-        return jsonwebtoken.sign({username, id}, webTokenSecret, 
-            { algorithm: 'RS256' },{ expiresIn: "1h" });
     }
 };
