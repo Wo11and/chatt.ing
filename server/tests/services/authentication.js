e463fcd006a4d1b89163571ab267fdfb87a8fc75
kaisiq <dahudohu@gmail.com>
2023-11-21 17:36:19 +0200

    Remove usage of dotenv, import db from knexconfig.js

--- server/tests/services/authentication.js
+++ server/tests/services/authentication.js
@@ -1,46 +1,28 @@
 import Knex from "knex";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const config = {
-    client: "pg",
-    connection: {
-        connectionString: process.env.DB_URL,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASS,
-        ssl: false,
-    },
-};
-const pg = Knex(config);
+import { database } from "../../knexconfig.js";
+const pg = Knex(database);
 const bcrypt_saltRounds = 10;
 
 import { AuthenticationService } from "../../services/authenticate.js";
 
-
-const auth = new AuthenticationService;
+const auth = new AuthenticationService();
 let username = "testuser8";
 let pass = "testpassword".toString();
 let wrongpass = "123".toString();
 
-
-
 const a = await auth.register(username, pass);
-try{
+try {
     const a = await auth.register(username, pass);
-}
-catch(err){ //must throw cuz user already in db
+} catch (err) {
+    //must throw cuz user already in db
     //nothing
     console.log("asd2");
-
 }
 const b = await auth.login(username, pass);
-try{
+try {
     const a = await auth.login(username, wrongpass);
-}
-catch(err){ //must throw (wrong password)
+} catch (err) {
+    //must throw (wrong password)
     console.log("asd3");
 }
