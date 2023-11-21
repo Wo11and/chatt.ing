ab2dee9cdadf9725091569beb116ecd27c585ae0
kaisiq <dahudohu@gmail.com>
2023-11-21 17:31:57 +0200

    Format code and resolve issues from PR

--- server/services/authenticate.js
+++ server/services/authenticate.js
@@ -1,141 +1,121 @@
 import jsonwebtoken from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import Knex from 'knex';
-import dotenv from "dotenv";
+import Knex from "knex";
 
- 
+import { database } from "../../knexconfig.js";
+const pg = Knex(database);
 
-dotenv.config();
-
-const config = {
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DB_URL,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    ssl: false,
-  },
-};
-const pg = Knex(config);
-  
-
-  
 const webTokenSecret = process.env.WEB_TOKEN_SECRET;
 const bcrypt_saltRounds = 10;
 
-
-
-export class AuthenticationService{
-
-
+export class AuthenticationService {
     checkTokenMiddleware = (req, res, next) => {
-        const header = req.headers['authorization'];
-    
-        if(typeof header !== 'undefined') {
-            const bearer = header.split(' ');
+        const header = req.headers["authorization"];
+        if (typeof header !== "undefined") {
+            const bearer = header.split(" ");
             const token = bearer[1];
-    
-            req.token = token;
-            next();
-        } else {
-            //If header is undefined return Forbidden (403)
-            res.sendStatus(403)
+            if (!token) {
+                return res.status(401).json({ message: "Unauthorized" });
+            }
+            //check if token is valid
+            jwt.verify(token, webTokenSecret, (err, decoded) => {
+                if (err) {
+                    return res.status(401).json({ message: "Unauthorized" });
+                }
+                req.token = token;
+                req.userData = decoded;
+                next();
+            });
         }
-    }
+        return res.status(401).json({ message: "Unauthorized" });
+    };
 
-    registerMiddleware(req, res, next){
+    registerMiddleware(req, res, next) {
         const username = req.body.username;
         const password = req.body.password;
-        try{
+        try {
             const token = this.register(username, password);
-            res.send(token);
-            res.end();  
-            next();
-        }
-        catch(err){
+            res.json({ token });
+        } catch (err) {
             console.log(err);
             console.log("cant register");
             res.writeHead(400);
         }
     }
 
-
-    loginMiddleware(req, res, next){
+    loginMiddleware(req, res, next) {
         const username = req.body.username;
         const password = req.body.password;
-        try{
-            const token = this.login(username,password);
-            res.send(token);
-        }
-        catch(err){
+        try {
+            const token = this.login(username, password);
+            res.json({ token });
+        } catch (err) {
             console.log(err);
             console.log("cant login");
+            res.writeHead(400);
         }
     }
 
-
-    async register(username, password){
-        try{
+    async register(username, password) {
+        try {
             //connect to db
-            await pg.raw('SELECT 1');
-            const user = await pg('users').where('username', username).first();
-            if (user !== undefined){
+            await pg.raw("SELECT 1");
+            const user = await pg("users").where("username", username).first();
+            if (user !== undefined) {
                 // If username exists, return an error or throw an exception
-                throw new Error('Username already exists');
+                throw new Error("Username already exists");
             }
             const hashedPassword = await bcrypt.hash(password, bcrypt_saltRounds);
             // Store hash and username in DB
             //...
-            const [userId] = await pg('users').insert({
-                username,
-                password: hashedPassword,
-            }, 'username');
+            const [userId] = await pg("users").insert(
+                {
+                    username,
+                    password: hashedPassword,
+                },
+                "username"
+            );
             console.log("Created user: " + userId);
-            const token = jsonwebtoken.sign({username, password},
-                webTokenSecret, {expiresIn: '1h'});
+            const token = jsonwebtoken.sign({ username, password }, webTokenSecret, {
+                expiresIn: "1h",
+            });
             return token;
         } catch (error) {
             // Handle any errors that occur during registration
-            console.error('Error during registration:', error.message);
+            console.error("Error during registration:", error.message);
             throw error;
         }
-    
     }
 
-    async login(username, password){
-        try{
-        //TODO: check if username exists -> if not - error
-        const user = await pg('users').where('username', username).first();
-        if (!user) {
-            // If username exists, return an error or throw an exception
-            throw new Error('Username doesnt exist');
-        }
-        //if exists check pass if not error
-        // hash == db password
-
-        bcrypt.compare(password, user.password, function(err, result) {
-            // result == true/false
-            if(err){
-                console.log(err);
-                throw err;
+    async login(username, password) {
+        try {
+            //check if user exists in db
+            const user = await pg("users").where("username", username).first();
+            if (!user) {
+                // If username exists, return an error or throw an exception
+                throw new Error("Username doesnt exist");
             }
-            if(!result){
-                throw new Error("Password not matching");
-            }
-        });
+            // hash == db password
 
-        //id -> db id
-        const token = jsonwebtoken.sign({username, password},
-            webTokenSecret, {expiresIn: '1h'});
-        return token;
-        }
-        catch(err){
+            bcrypt.compare(password, user.password, function (err, result) {
+                // result == true/false
+                if (err) {
+                    console.log(err);
+                    throw err;
+                }
+                if (!result) {
+                    throw new Error("Password not matching");
+                }
+            });
+
+            //id -> db id
+            const token = jsonwebtoken.sign({ username, password }, webTokenSecret, {
+                expiresIn: "1h",
+            });
+            return token;
+        } catch (err) {
             console.log("Error during login: ", err);
             throw err;
         }
-
     }
-};
+}
