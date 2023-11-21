b48fc80b24b7f6dfd3bd4d6e38a8db5153aaef4e
kaisiq <dahudohu@gmail.com>
2023-11-21 17:35:46 +0200

    Format code; resolve PR issues

--- server/index.js
+++ server/index.js
@@ -5,53 +5,47 @@ import { createServer } from "node:http";
 import "dotenv/config";
 import { AuthenticationService } from "./services/authenticate.js";
 
-
 const app = express();
 app.use(cors());
 const server = createServer(app);
 const io = new Server(server, {
-	cors: {
-		origin: process.env.CLIENT_ADRESS,
-	},
+    cors: {
+        origin: process.env.CLIENT_ADRESS,
+    },
 });
 
 const port = process.env.SERVER_PORT;
-const auth = new AuthenticationService;
+const auth = new AuthenticationService();
 
 app.use(express.json());
 
+app.post("/login", auth.loginMiddleware);
+
+app.post("/register", auth.loginMiddleware);
+
+//check token fore every http request to the backend below this line
+app.use(auth.checkTokenMiddleware);
+
 app.get("/", (req, res) => {
-	res.sendFile('/home/kaisiq/Programming/chatt.ing/frontend/index.html');
+    res.send({ message: "Hello World!" });
 });
 
-
-app.post("/login", (req,res,next) => {
-	auth.loginMiddleware(req,res,next);
+io.on("connection", function (socket) {
+    console.log("user connected");
+    socket.on("chat message", function (msg) {
+        io.emit("chat message", msg);
+    });
+    socket.on("disconnect", function () {
+        console.log("user disconnected");
+    });
 });
-
-
-app.post("/register", (req,res,next) => {
-	auth.registerMiddleware(req,res,next)
-});
-
-
-io.on('connection', function(socket){
-	console.log('user connected');
-	socket.on('chat message', function(msg){
-	  io.emit('chat message', msg);
-	});
-	socket.on('disconnect', function(){
-	  console.log('user disconnected');
-	});
-});
-
 
 app.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
 
 io.on("connection", (socket) => {
-	console.log("a user connected", socket.handshake.auth);
+    console.log("a user connected", socket.handshake.auth);
 });
 
 // verify the jwt token upon connecting
@@ -76,16 +70,16 @@ io.on("connection", (socket) => {
 let activeUsers = [];
 
 io.on("connection", (socket) => {
-	socket.emit("users", activeUsers);
-	const newEntry = {
-		userId: socket.handshake.auth.id,
-		username: socket.handshake.auth.name,
-		socketId: socket.id,
-	};
-	activeUsers.push(newEntry);
-	socket.broadcast.emit("users", activeUsers);
-	socket.on("disconnect", () => {
-		activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
-		socket.broadcast.emit("users", activeUsers);
-	});
+    socket.emit("users", activeUsers);
+    const newEntry = {
+        userId: socket.handshake.auth.id,
+        username: socket.handshake.auth.name,
+        socketId: socket.id,
+    };
+    activeUsers.push(newEntry);
+    socket.broadcast.emit("users", activeUsers);
+    socket.on("disconnect", () => {
+        activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+        socket.broadcast.emit("users", activeUsers);
+    });
 });
